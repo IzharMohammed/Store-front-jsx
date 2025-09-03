5ebdc3a5873eeedbcda911d6719c843b9d24fa63
IzharMohammed <izharmohammed21@gmail.com>
2025-09-04 00:17:46 +0530

    next config to js

---
+++ next.config.js
@@ -0,0 +1,24 @@
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.pexels.com",
+      },
+      {
+        protocol: "https",
+        hostname: "plus.unsplash.com",
+      },
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+      },
+      {
+        protocol: "https",
+        hostname: "www.pexels.com",
+      },
+    ],
+  },
+};
+
+export default nextConfig;
