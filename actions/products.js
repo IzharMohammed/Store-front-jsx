3831afebc224bb13b1648763321e484e62a3ad06
IzharMohammed <izharmohammed21@gmail.com>
2025-09-26 19:35:20 +0530

    Refactor:cleanup

--- actions/products.js
+++ actions/products.js
@@ -5,45 +5,6 @@ import { cookieManager } from "@/utils/authTools";
 const API_KEY = process.env.BACKEND_API_KEY || "";
 const BACKEND_URL = process.env.BACKEND_URL || "";
 
-// export async function getProducts() {
-//   try {
-//     const userData = await cookieManager.getAuthUser();
-
-//     const headers = {
-//       "Content-Type": "application/json",
-//       "x-api-key": API_KEY,
-//     };
-
-//     // Add custom headers if user is authenticated
-//     if (userData) {
-//       headers["x-customer-id"] = userData.id;
-//     }
-
-//     const response = await fetch(`${BACKEND_URL}/v1/products`, {
-//       method: "GET",
-//       headers,
-//       cache: "no-store", // Ensure fresh data on each request
-//       next: {
-//         tags: ["products"],
-//       },
-//     });
-
-//     if (!response.ok) {
-//       const errorData = await response.json().catch(() => ({}));
-//       return {
-//         success: false,
-//         error:
-//           errorData.message ||
-//           `Server error: ${response.status}. Please try again.`,
-//       };
-//     }
-
-//     return response.json();
-//   } catch (error) {
-//     console.error("Error fetching cart:", error);
-//   }
-// }
-
 export async function getProducts(filters = {}) {
   try {
     const userData = await cookieManager.getAuthUser();
