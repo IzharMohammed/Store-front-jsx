b9697651ac89e7e248d068f586b2273f3c9f6dc8
IzharMohammed <izharmohammed21@gmail.com>
2025-09-26 20:13:21 +0530

    Refactor:cleanup

--- components/product/products.jsx
+++ components/product/products.jsx
@@ -38,7 +38,8 @@ export async function Products() {
         </div>
 
         {/* Products Grid */}
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 md:gap-6">
+        {/* <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 md:gap-6"> */}
+        <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 xl:grid-cols-5 gap-3 md:gap-4">
           {products.map((product) => {
             // Find cart item for this product
             const cartItem = cartItems.find(item => item.productId === product.id);
